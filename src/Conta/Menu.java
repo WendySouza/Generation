5aa7606bc0e6915e5bc974695969384677de997d
Wendy Souza <contato.wendy@hotmail.com>
2024-06-12 17:02:54 -0300

    Adicionado 'Conta.Model'.

--- src/Conta/Menu.java
+++ src/Conta/Menu.java
@@ -2,13 +2,20 @@ package Conta;
 
 import java.util.*;
 
+import Conta.Model.Conta;
 import Conta.Util.cores;
 
 public class Menu {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Conta c1 = new Conta(1, 123, 1, "Adriana", 10000.0f);
+		c1.visualizar();
+		c1.sacar(12000.0f);
+		c1.visualizar();
+		c1.depositar(5000.0f);
+		c1.visualizar();
+		
 		Scanner leia = new Scanner(System.in);
 
 		int opcao;
